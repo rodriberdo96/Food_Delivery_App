12baf9b974311f0626e008b2d06b2104e0c1c996
Rodri Berdomas <rodriberdomas@gmail.com>
2025-12-15 12:07:49 -0300

    Move MongoDB URI to env variables

---
+++ backend/config/db.js
@@ -0,0 +1,6 @@
+import mongoose, { mongo } from "mongoose";
+import dotenv from 'dotenv';
+
+export const connectDB= async ()=>{
+    await mongoose.connect(process.env.MONGO_URI).then(()=> console.log("DB connected"))
+}
