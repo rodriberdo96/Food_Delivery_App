aa2f59542ccbcf40c0a027ebf59b5d16a6fb26b5
Rodri Berdomas <rodriberdomas@gmail.com>
2026-07-10 10:25:13 -0300

    .

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Navbar } from './components/Navbar/Navbar'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, useLocation } from 'react-router-dom'
 import { Home } from './pages/Home/Home'
 import { Cart } from './pages/Cart/Cart'
 import { PlaceOrder } from './pages/PlaceOrder/PlaceOrder'
@@ -8,9 +8,15 @@ import { Footer } from './components/Footer/Footer'
 import { LoginPopup } from './components/LoginPopup/LoginPopup'
 import { Verify } from './pages/Verify/Verify'
 import { MyOrders } from './pages/MyOrders/MyOrders'
+import { useEffect } from 'react'
 
 const App = () => {
   const [showLogin, setShowLogin] = React.useState(false)
+  const { pathname } = useLocation()
+
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [pathname])
 
   return (
     <div className="app-wrapper">
